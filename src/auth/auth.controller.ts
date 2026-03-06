7017bddaab331f6791e4a2713ac0c065e746d144
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2026-03-06 12:21:50 +0530

    Update auth.controller.ts

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,32 +1,42 @@
-import { Body, Controller, Headers, Post, Req, UnauthorizedException } from "@nestjs/common";
+import { Body, Controller, Headers, Post, UnauthorizedException } from "@nestjs/common";
 import { AuthService } from "./auth.service";
 import { RequestOTPDTO } from "./dtos/request-otp.dto";
 import { VerifyOTPDTO } from "./dtos/verify-otp.dto";
-import type { Request } from 'express';
+import { ClientInfo } from "../common/decorators/client-info.decorator";
 
 @Controller('auth')
 export class AuthController {
-    constructor(private readonly authService: AuthService) {}
+    constructor(private readonly authService: AuthService) { }
 
     @Post('request-otp')
-    requestOTP(@Body() dto: RequestOTPDTO, @Req() req: Request) {
-        const ipAddress = req.ip;
-        const userAgent = req.headers['user-agent'] || '';
-        return this.authService.RequestOTP(dto.email, ipAddress, userAgent);
+    requestOTP(
+        @Body() dto: RequestOTPDTO,
+        @ClientInfo() client
+    ) {
+        return this.authService.RequestOTP(
+            dto.email,
+            client.ipAddress,
+            client.userAgent
+        );
     }
 
     @Post('verify-otp')
     verifyOTP(
         @Body() dto: VerifyOTPDTO,
         @Headers('authorization') authHeader: string,
-        @Req() req: Request
+        @ClientInfo() client
     ) {
         if (!authHeader || !authHeader.startsWith('Bearer ')) {
             throw new UnauthorizedException("Invalid or missing token");
         }
+
         const token = authHeader.split(' ')[1];
-        const ipAddress = req.ip;
-        const userAgent = req.headers['user-agent'] || '';
-        return this.authService.VerifyOTP(token, dto.otp, ipAddress, userAgent);
+
+        return this.authService.VerifyOTP(
+            token,
+            dto.otp,
+            client.ipAddress,
+            client.userAgent
+        );
     }
 }
